c2f5afa70e8200fea3ea780450fec80c1a9a8580
sorenmulli <swholm@protonmail.com>
2024-04-24 10:20:05 +0200

    Add new models, fix model details

--- dano_leaderboard/frontend/details.py
+++ dano_leaderboard/frontend/details.py
@@ -3,56 +3,9 @@ from pathlib import Path
 
 from ..constants import ASSETS_PATH
 
-_MODELS = [
-    "OpenAI GPT 4",
-    "OpenAI GPT 4 Turbo",
-    "OpenAI GPT 3.5 Turbo",
-    "OpenAI GPT 3.5 Turbo Instruct",
-    "Google Gemini Pro",
-    "SOLAR 10.7B Instruct",
-    "Claude Sonnet",
-    "Claude Haiku",
-    "Mixtral (@ Groq)",
-    "Qwen1.5 7B chat",
-    "OpenAI Davinci 003",
-    "Heidrun Mistral 7B Chat",
-    "mGPT 13B",
-    "Mistral 7B Instruct (v0.2)",
-    "Gemma 7B instruct",
-    "Mistral 7B Instruct",
-    "Heidrun Mistral 7B Base",
-    "Munin NeuralBeagle 7B",
-    "Gemma 7B",
-    "Kanelsnegl (v. 0.2)",
-    "DanskGPT-tiny Chat",
-    "LlaMa 2 13B Chat",
-    "Hestenettet LM",
-    "OpenAI Davinci 002",
-    "Munin 7B Alpha",
-    "Mistral 7B",
-    "LlaMa 2 7B Chat",
-    "Danoliterate Mistral 7B",
-    "GPT-Sw3 6.7B Instruct (v. 2)",
-    "NB GPT-J NorPaca",
-    "LlaMa 2 13B",
-    "GPT-Sw3 6.7B (v. 2)",
-    "SOLAR 10.7B",
-    "LlaMa 2 7B",
-    "OpenAI Babbage 002",
-    "Danoliterate LlaMa 2 7B",
-    "DanskGPT-tiny",
-    "GPT 7B Nordic pre.",
-    "GPT Neo Danish",
-    "mGPT",
-    "NB GPT-J",
-    "Phi-2",
-    "Danoliterate 7B Baseline",
-    "GPT Neox Da.",
-    "Constant Baseline",
-]
 MODELS = []
-for model in _MODELS:
-    with open(ASSETS_PATH / "models" / f"{model}.yaml", "r") as file:
+for path in (ASSETS_PATH / "models").glob("*.yaml"):
+    with path.open("r") as file:
         MODELS.append(yaml.safe_load(file))
 MODEL_DICT = {model["model"]: model for model in MODELS}
 DIMENSIONS_TO_HIDE_MODELS = {
