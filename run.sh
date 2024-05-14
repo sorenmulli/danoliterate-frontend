099725546ad9c187e7d860f4ff83ffa54319979d
sorenmulli <swholm@protonmail.com>
2024-05-14 02:52:33 +0200

    Add more models to survey

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-START_PAGE="streamlit-app/🇩🇰_Hello.py"
+START_PAGE="streamlit-app/✨_Hello.py"
 REPO_DIR=$(dirname "$0")
 
 /home/swiho/.pyenv/versions/server3.11/bin/python -m streamlit run $REPO_DIR/$START_PAGE
