3428f8926d5f3800b4e25fc95486cd83848836cc
sorenmulli <swholm@protonmail.com>
2024-10-09 14:35:57 +0200

    Add bradley terry model and full text

---
+++ dano_leaderboard/frontend/analysis/survey_dataset.py
@@ -0,0 +1,216 @@
+from itertools import combinations
+
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+import numpy as np
+from scipy.stats import norm
+from scipy import stats
+import networkx as nx
+from matplotlib import pyplot as plt
+
+
+RENAME = {
+    None: "       N/A",
+    "Mand": "Male",
+    "Kvinde": "Female",
+    "Professionel erfaring": "  Professional",
+    "Mindre erfaring": "    Lesser",
+    "Større erfaring på hobby-niveau": "   Greater",
+    "Ingen erfaring": "     None",
+    "Dansk": " Danish",
+    "Et andet sprog": " Another Language",
+}
+
+BASE = 10
+PRETTY_NAMES = {
+    "mhenrichsen/danskgpt-tiny-chat": "DanskGPT-tiny Chat",
+    "Qwen/Qwen1.5-7B-Chat": "Qwen1.5 7B Chat",
+    "RJuro/munin-neuralbeagle-7b": "Munin NeuralBeagle",
+    "upstage/SOLAR-10.7B-Instruct-v1.0": "SOLAR 10.7B In.",
+    "mistralai/Mistral-7B-Instruct-v0.2": "Mistral 7B In. v0.2",
+    "llama3-8b-8192": "LlaMa 3 8B In.",
+    "google/gemma-7b-it": "Gemma 7B In.",
+    "llama3-70b-8192": "LlaMa 3 70B",
+    "mixtral-8x7b-32768": "Mixtral 8x7B",
+    "Mabeck/Heidrun-Mistral-7B-chat": "Heidrun 7B Chat",
+    "Nexusflow/Starling-LM-7B-beta": "Starling 7B",
+    "gpt-3.5-turbo-0125": "GPT 3.5 Turbo",
+    "claude-3-sonnet-20240229": "Claude Sonnet",
+    "gpt-4-turbo": "GPT 4 Turbo 2024",
+    "claude-3-haiku-20240307": "Claude Haiku",
+    "gemini-pro": "Gemini Pro",
+    "gpt-4o": "GPT 4o",
+    "claude-3-opus-20240229": "Claude Opus",
+}
+
+
+def plot_demographics(
+    ax, df, column, title, valuesort=False, rotation=45, ylim=True, density=False
+):
+    total = len(df)
+    df = df.copy()
+    if column != "user-age":
+        df[column] = df[column].apply(lambda k: RENAME.get(k, k))
+
+    sns.countplot(
+        data=df,
+        x=column,
+        order=sorted(x for x in df[column].unique() if not pd.isna(x))
+        if valuesort
+        else df[column].value_counts().index,
+        ax=ax,
+    )
+    ax.set_xlabel("")
+    ax.set_title(title)
+    ax.set_ylabel("Density" if density else "Count")
+    if ylim:
+        ax.set_ylim(0, total + 10)
+    ax.tick_params(axis="x", rotation=rotation)
+
+    # Adding percentage annotations on top of the bars
+    for p in ax.patches:
+        height = p.get_height()
+        percentage = f"{height / total:.0%}"
+        ax.annotate(
+            percentage,
+            (p.get_x() + p.get_width() / 2.0, height),
+            ha="center",
+            va="center",
+            xytext=(0, 9),
+            textcoords="offset points",
+        )
+
+
+def compute_bradley_terry(df: pd.DataFrame) -> pd.DataFrame:
+    ptbl_a_win = pd.pivot_table(
+        df[df["prefer"] == "A"],
+        index="model_A",
+        columns="model_B",
+        aggfunc="size",
+        fill_value=0,
+    )
+
+    ptbl_b_win = pd.pivot_table(
+        df[df["prefer"] == "B"],
+        index="model_A",
+        columns="model_B",
+        aggfunc="size",
+        fill_value=0,
+    )
+    ptbl_win = ptbl_a_win * 2 + ptbl_b_win.T * 2
+
+    models = pd.Series(np.arange(len(ptbl_win.index)), index=ptbl_win.index)
+
+    n_combinations = len(models) * (len(models) - 1) * 2
+    X = np.zeros((n_combinations, len(models)))
+    Y = np.zeros(n_combinations)
+
+    reward = np.log(BASE)
+    i = 0
+    sample_weights = []
+    for m_a in ptbl_win.index:
+        for m_b in ptbl_win.columns:
+            if m_a == m_b:
+                continue
+            X[i, models[m_a]] = reward
+            X[i, models[m_b]] = -reward
+            Y[i] = 1.0
+            sample_weights.append(ptbl_win.loc[m_a, m_b])
+
+            X[i + 1, models[m_a]] = reward
+            X[i + 1, models[m_b]] = -reward
+            Y[i + 1] = 0.0
+            sample_weights.append(ptbl_win.loc[m_b, m_a])
+            i += 2
+    model = sm.GLM(Y, X, family=sm.families.Binomial(), freq_weights=sample_weights)
+    results = model.fit()
+    return pd.DataFrame(
+        {"BT coefficient": results.params, "SE": results.bse}, index=models.index.values
+    ).sort_values(by="BT coefficient", ascending=False)
+
+
+def visualize_bradley_terry_ranking(std_bt_board: pd.DataFrame, alpha=0.05):
+    min_ci = -1
+    max_ci = 1
+    bt_index = std_bt_board["BT coefficient"].apply(
+        lambda x: (x - min_ci) / (max_ci - min_ci)
+    )
+
+    combination_significant = {}
+    ps = []
+    for one, other in combinations(std_bt_board.index, 2):
+        coef1, se1 = std_bt_board.loc[one]
+        coef2, se2 = std_bt_board.loc[other]
+        diff = coef1 - coef2
+        se_diff = np.sqrt(se1**2 + se2**2)
+        ps.append(2 * (1 - norm.cdf(abs(diff / se_diff))))
+
+    ps = stats.false_discovery_control(ps, method="bh")
+    for p, (one, other) in zip(ps, combinations(std_bt_board.index, 2), strict=True):
+        combination_significant[(one, other)] = p < alpha
+
+    G = nx.Graph()
+    G.add_nodes_from(
+        [node for node, _ in std_bt_board.sort_values("BT coefficient").iterrows()]
+    )
+    for (model1, model2), significant in combination_significant.items():
+        if not significant and model1 in G.nodes and model2 in G.nodes:
+            G.add_edge(model1, model2)
+
+    radius_increase = 1.2
+    pos = nx.circular_layout(G, center=[0, 0])
+    label_pos = {
+        name: coord * (np.linalg.norm(coord) * radius_increase) / np.linalg.norm(coord)
+        for name, coord in pos.items()
+    }
+
+    fig, axis = plt.subplots(figsize=(7, 7))
+
+    nx.draw_networkx_labels(
+        G,
+        label_pos,
+        {
+            node: " ".join(
+                "\n" + ch if i % 2 else ch
+                for i, ch in enumerate(PRETTY_NAMES[node].replace("-", " ").split())
+            )
+            for node in G
+        },
+        font_size=10,
+        font_family="serif",
+    )
+    nx.draw_networkx_labels(
+        G,
+        pos,
+        font_size=12,
+        labels={n: int(bt_index[n] * 100) for n in G},
+        font_family="serif",
+    )
+
+    vmax, vmin = (1.1, -0.1)
+    nx.draw_networkx(
+        G,
+        pos,
+        labels={},
+        node_color=[bt_index[node] for node in G.nodes],
+        cmap=plt.cm.Spectral,
+        connectionstyle="arc3,rad=-0.2",
+        arrows=True,
+        vmax=vmax,
+        vmin=vmin,
+        node_size=1000,
+    )
+    plt.axis("off")
+    axis.text(
+        0,
+        0,
+        "Human Feedback Ranking\nBradley-Terry Model",
+        horizontalalignment="center",
+        verticalalignment="center",
+        fontsize=15,
+    )
+    axis.set_xlim([1.2 * x for x in axis.get_xlim()])
+    axis.set_ylim([1.2 * y for y in axis.get_ylim()])
+    fig.tight_layout()
+    return fig
--- dano_leaderboard/frontend/articles/survey_blog.py
+++ dano_leaderboard/frontend/articles/survey_blog.py
@@ -1,21 +1,146 @@
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
+from threading import Lock
+
 import streamlit as st
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from datasets import load_dataset
 
-from datasets import Dataset, load_dataset
+plt_lock = Lock()
 
-from dano_leaderboard.constants import ASSETS_PATH
 
 from .base import BaseArticle
+from ..analysis.survey_dataset import (
+    plot_demographics,
+    compute_bradley_terry,
+    visualize_bradley_terry_ranking,
+)
+
 
 @st.cache_data
-def get_dataset() -> Optional[Dataset]:
+def get_dataset() -> Optional[pd.DataFrame]:
     try:
-        return load_dataset("sorenmulli/danoliterate-survey-answers", split="train")
+        return pd.DataFrame(
+            load_dataset("sorenmulli/danoliterate-survey-answers", split="train")
+        )
     except ConnectionError as error:
         print("Got connection error:", error)
         return None
+
+
+def display_results(survey_df):
+    survey_models = list(
+        set(np.concatenate((survey_df["model_A"], survey_df["model_B"])))
+    )
+    st.write(
+        "So far, we have collected `%i` A/B tests for `%i` models from `%i` unique users"
+        % (len(survey_df), len(survey_models), len(survey_df["session-id"].unique()))
+    )
+    grouped_df = survey_df.groupby("session-id").first().reset_index()
+    st.write(
+        "Many of these are male, young, and have a high level of experience in GLLMs:"
+    )
+    with plt_lock:
+        fig, axes = plt.subplots(2, 2, figsize=(6, 6))
+        plot_demographics(
+            axes[0, 0],
+            grouped_df,
+            "user-gender",
+            "Gender Distribution",
+            density=True,
+            valuesort=True,
+        )
+        plot_demographics(
+            axes[0, 1], grouped_df, "user-age", "Age Group Distribution", valuesort=True
+        )
+        plot_demographics(
+            axes[1, 0],
+            grouped_df,
+            "user-language",
+            "1st Language Distribution",
+            valuesort=True,
+        )
+        plot_demographics(
+            axes[1, 1],
+            grouped_df,
+            "user-experience",
+            "User Experience Distribution",
+            valuesort=True,
+        )
+        fig.tight_layout()
+        st.pyplot(fig)
+
+    st.write("Future expansion of the survey should target a wider demographic.")
+    st.write("Let's go to some high-level impressions from their answers:")
+
+    with plt_lock:
+        fig, axes = plt.subplots(2, 2, figsize=(6, 6))
+        plot_demographics(axes[0][0], survey_df, "prefer", "Global Preferences")
+        plot_demographics(
+            axes[0][1],
+            pd.DataFrame(
+                {
+                    "likert": survey_df["likert-B"].tolist()
+                    + survey_df["likert-A"].tolist()
+                }
+            ),
+            "likert",
+            "Global Likert Scores",
+            valuesort=True,
+        )
+        plot_demographics(
+            axes[1][0],
+            pd.DataFrame(
+                {
+                    "n_seen_prompts": [
+                        len(prompts)
+                        for prompts in survey_df["seen_prompts"]
+                        if prompts is not None
+                    ]
+                }
+            ),
+            "n_seen_prompts",
+            "Number of Seen Prompts",
+            valuesort=True,
+        )
+        plot_demographics(axes[1][1], survey_df, "index", "A/B Test Session Index")
+        fig.tight_layout()
+        st.pyplot(fig)
+    st.write(
+        "On the inputs, people are able to decide a winner in most cases and, nicely, give well symmetrically distributed scores. "
+        "Most users see the required 3 prompts, other keep going and see more before choosing a winner. "
+        "Finally, we see that most results come from the first A/B test of a user with only about half of users completing more than one: "
+        " though most that complete more than one then also complete all suggested four"
+    )
+    st.write(
+        "But the most interesting thing: *The ranking!* "
+        f"We have a dataset of `{len(survey_df)}` pairwise preferences and want to transform that into one overall ranking."
+        "This classical problem in the literature of learning to rank is approached by the authors of the LmSys Chatbot Arena "
+        "using the Bradley-Terry model [chiang-et-al]. The central modelling decision is to estimate the probablity that model $m_A$ "
+        "is preferred over model $m_B$ logistically as"
+    )
+    st.latex(r"P(m_A \succ m_B) = \frac{1}{1 + \exp(-(\theta_{m_A} - \theta_{m_B}))}.")
+    st.write(
+        r"Where $\theta_{m_A}, \theta_{m_B}$ are learned nonparametrically from the data using maximum likelihood estimation "
+        " following Section 4 and Appendix B in [chiang-et-al] which we verified using their open-source implementation."
+    )
+    bradley_terry_results = compute_bradley_terry(survey_df)
+    st.write(
+        r"The coefficient $\theta_m$ thus induces a ranking (higher is better) as well as an uncertainty, shown below"
+    )
+    # TODO: Consider to prettify table
+    st.dataframe(bradley_terry_results)
+    st.write(
+            "Below these results are visualized. They are scaled from [-1, 1] to [0, 100] and pairwise statistical significance is shown: "
+            "If there is no significant difference at $\\alpha=0.05$ between two models, their nodes are connected. "
+            "The pairwise tests have been Benjamini-Hochberg corrected for multiple comparisons."
+    )
+    with plt_lock:
+        fig = visualize_bradley_terry_ranking(bradley_terry_results)
+        st.pyplot(fig)
 
 
 @dataclass
@@ -29,25 +154,28 @@ class SurveyArticle(BaseArticle):
         st.write(
             """
 The Danoliterate benchmark automatically evaluates Generative Large, Language models on Danish.
-Analysis suggests that the different automatic evaluation scenarios are correlated with each other into one, underlying factor of Danoliteracy.
-Though encouraging, there is a harder question:
-Is this common benchmark dimension correspondent to a meaningful factor of Danish capabilities in GLLMs, a sort of Danoliteracy $g$-factor?
+Analyzing these results, we have found that the different evaluation scenarios are correlated with each other into one, underlying factor of Danoliteracy.
+This is encouraging for the benchmarking of GLLMs in Danish, but it is not enough:
+Does our found factor actually descirbe something meaningful?
+Have we found a real-world dimension of Danish capabilities, a sort of Danoliteracy $g$-factor?
 
 
 Let's ask Danish speakers!
 
-Inspired by the popular English LmSys Chatbot Arena [lmsys], we made an interactive survey site allowing Danish speakers to explore different models and prompts,
+Inspired by the popular, primarily English-language [LmSys Chatbot Arena](https://lmarena.ai/) [lmsys], we made an interactive survey site allowing Danish speakers to explore different models and prompts,
 giving their own judgement of model quality.
 
 The survey is still live at this page under ''Spørgeskema'', and if you speak Danish, please check it out!
 """
         )
-        st.link_button("Check out the survey", "https://danoliterate.compute.dtu.dk/Spørgeskema")
+        st.link_button(
+            "Check out the survey", "https://danoliterate.compute.dtu.dk/Spørgeskema"
+        )
 
-        st.subheader("2. Methodology")
+        st.subheader("2. Survey Design")
         st.write(
-"""
-Instead of hosting GLLMs live like the English Chatbot Arena, 100 Danish prompts were produced.
+            """
+Instead of hosting GLLMs live like the English Chatbot Arena, 100 Danish prompts were pre-generated.
 These were based on 100 popular Generative AI use-cases according to [zao-sanders].
 For each of these, we translated the category and use-case to Danish and manually produced a Danish-language
 prompt corresponding to this description.
@@ -67,34 +195,78 @@ The user is required to see outputs on at least three prompts before they can fi
 
 3. A text box.
 
-All are optional, containing "I don't know" as an option.
+All are optional, containing "I don't know" or leaving it empty as options.
 
-The user have a chance to get revealed what models they saw, locking their answers, before they
-can go on to the next model.
+After filling out their judgement, the user can choose to get revealed what models they saw, locking their answers,
+and continue to a next A/B test.
 
 All data is saved live during interaction.
-""")
-
+"""
+        )
 
         st.subheader("3. Survey Results")
-        # TODO: Make PNG
-        # st.image(str(ASSETS_PATH /"result-significance-human-feedback.pdf"), caption="hej")
         with st.spinner("Loading newest answers..."):
-            dataset = get_dataset()
-        st.write(f"Number of answers: {None if dataset is None else len(dataset)}")
-        st.info("More results are coming soon")
+            survey_df = get_dataset()
+        if survey_df is not None:
+            display_results(survey_df)
 
-        st.subheader("4. See more")
-        st.link_button("Check out the dataset of prompts", "https://huggingface.co/datasets/sorenmulli/danoliterate-survey-prompts",)
-        st.link_button("Check out the dataset of replies", "https://huggingface.co/datasets/sorenmulli/danoliterate-survey-answers",)
-        st.link_button("Try the survey yourself", "https://danoliterate.compute.dtu.dk/Spørgeskema")
-        st.link_button("Read more about the Danoliterate project", "https://danoliterate.compute.dtu.dk")
+        st.subheader("4. Conclusions")
         st.write(
+            """
+The results show a significant ranking with different tiers of models.
+Claude models from Anthropic AI generally rank higher in the human preferences than in the automatic benchmark.
+Like the automatic benchmark, the open-source models lack behind OpenAI, Google and Anthropic GLLMs.
+
+The comparison to the automatic benchmark is a crucial result of the survey: Can we trust scenario-based
+benchmarking to give a good indication of human preferences?
+Not too bad, it seems!
+For the October 2024 version of automatic and survey benchmark results, we see a rank correlation of $\\rho \\sim 0.8.$.
+Automatic benchmarking appears to be a useful tool for this technology.
+
+However, the survey is small-scale and with a narrow user group:
+With more responses and respondents that more accurately reflect Danish speakers and thus more accurately
+reflect those that will be impacted by this technology, we can learn much more about how humans preference
+and GLLM performance interact in Danish.
+
+Amongst the many potential research question with more data are:
+
+- How do gender, age, and experience in GLLMs impact preferences?
+
+- Do users prefer some models for some categories of prompts and others for other tasks?
+
+- When are users in doubt and why?
+
+- Are there signals in model outputs that correlate with user preference?
+
+So please, help improve our understanding of this disruptive technology in Danish by taking the survey
+and spreading the word!
 """
+        )
+
+        st.subheader("5. See more")
+        st.link_button(
+            "Check out the dataset of prompts",
+            "https://huggingface.co/datasets/sorenmulli/danoliterate-survey-prompts",
+        )
+        st.link_button(
+            "Check out the dataset of replies",
+            "https://huggingface.co/datasets/sorenmulli/danoliterate-survey-answers",
+        )
+        st.link_button(
+            "Try the survey yourself", "https://danoliterate.compute.dtu.dk/Spørgeskema"
+        )
+        st.link_button(
+            "Read more about the Danoliterate project",
+            "https://danoliterate.compute.dtu.dk",
+        )
+        st.write(
+            """
 Literature
 
-- [lmsys]  Lianmin Zheng, Wei-Lin Chiang, Ying Sheng et al. 2023. Judging LLM-as-a-Judge with MT-Bench and Chatbot Arena. arXiv:2306.05685.
+- [lmsys]  Lianmin Zheng, Wei-Lin Chiang, Ying Sheng et al. 2023. Judging LLM-as-a-Judge with MT-Bench and Chatbot Arena. [arXiv:2306.05685](https://arxiv.org/abs/2306.05685).
 
-- [zao-sanders] Marc Zao-Sanders. 2024. How people are really using GenAI. Harvard Business Review.
+- [chiang-et-al] Wei-Lin Chiang, Lianmin Zheng, Yiang Sheng et al. 2024. Chatbot Arena: An Open Platform for Evaluating LLMs by Human Preference. [arXiv:2403.04132](https://arxiv.org/abs/2403.04132).
+
+- [zao-sanders] Marc Zao-Sanders. 2024. How people are really using GenAI. [Harvard Business Review](https://hbr.org/2024/03/how-people-are-really-using-genai).
 """
-)
+        )
